958416b63f8313cecbb8504d42206c4bc9935099
UmedMuzl <ahmedmslh@gmail.com>
2026-09-10 21:20:28 -0500

    init

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+echo "Creating ./bin directory..."
+mkdir -p ./bin
+
+echo "Running RecompModTool..."
+./RecompModTool ./mod.toml ./bin
+
+echo "Zipping output file into ./bin..."
+zip -j ./bin/lanky_arms.zip ./bin/lanky_arms.nrm
+
+echo "Complete"
